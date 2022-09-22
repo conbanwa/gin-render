96586f047d05a0c86f85aff2f611e7675501db7c
conbanwa <j6j7@qq.com>
2022-09-22 16:24:15 +0800

    initial commit
    Signed-off-by: conbanwa <j6j7@qq.com>

---
+++ main.go
@@ -0,0 +1,59 @@
+package render
+
+import (
+	"time"
+)
+
+// @version 0.0.1
+// @description last updated at 9/22/2022 4:24:14 PM
+
+//SetMapValue set map value
+func (c *Context) SetMapValue(key string) {
+	if c.Keys == nil {
+		c.Keys = make(map[string]interface{})
+	}
+	c.Keys[mapField] = key
+}
+
+//SetTimerValue set timer value
+func (c *Context) SetTimerValue() {
+	if c.Keys == nil {
+		c.Keys = make(map[string]interface{})
+	}
+	c.Keys[timerField] = time.Now().UnixNano()
+}
+
+// call c.SetKeyValue before call this function
+func (c *Context) Render(obj interface{}, err error) {
+	field, ok := c.Keys[mapField].(string)
+	if !ok {
+		if _, ok := obj.([]interface{}); ok {
+			field = listField
+		} else {
+			field = resultField
+		}
+	}
+	r := BaseResult{
+		Code: 0,
+	}
+	if err != nil {
+		r.Msg = err.Error()
+		r.Code = 1
+	}
+	if field == noField {
+		r.Data = obj
+	} else {
+		r.Data = map[string]interface{}{field: obj}
+	}
+	var timer int64
+	if start, ok := c.Keys[timerField].(int64); ok {
+		timer = time.Now().UnixNano() - start
+		r.TimeCost = timer
+	}
+	c.JSON(200, r)
+}
+
+func (c *Context) JsonDirect(rows interface{}, err error) {
+	c.SetMapValue(noField)
+	c.Render(rows, err)
+}
---
+++ model.go
@@ -0,0 +1,33 @@
+package render
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	mapField    = "ctx"
+	noField     = "result"
+	resultField = "result"
+	listField   = "list"
+	methodField = "method"
+	pathField   = "path"
+	timerField  = "timer"
+)
+
+// Context ext content struct
+type (
+	Context struct {
+		*gin.Context
+	}
+	// HandlerFunc achieves gin.HandlerFunc
+	HandlerFunc func(*Context) string
+	BaseResult  struct {
+		Code     int         `json:"code,omitempty"`
+		Msg      string      `json:"msg,omitempty"`
+		Data     interface{} `json:"data,omitempty"`
+		TimeCost int64       `json:"timeCost,omitempty"`
+		Total    int64       `json:"total,omitempty"`
+		// Key    string            `json:"key,omitempty"`
+		// Detail map[string]string `json:"detail,omitempty"`
+	}
+)
